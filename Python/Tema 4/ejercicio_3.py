4f612a0ba1262882a5cb6426074f33b4c2ee9621
Chuchedark <105623174+Chuchedark@users.noreply.github.com>
2022-08-28 22:39:41 -0500

    Update ejercicio_3.py

--- Python/Tema 4/ejercicio_3.py
+++ Python/Tema 4/ejercicio_3.py
@@ -2,4 +2,4 @@ rango_1_100 = range(1,101)
 
 # Se muestra el rango de números desde 100 hasta 1
 for i in reversed(rango_1_100):
-    print(f"- {i}")
+    print(f"- {i}") #se agrega un signo de menos, lo cual genera que la lista sea dada en números negativos, no se si esté correcto o no.
